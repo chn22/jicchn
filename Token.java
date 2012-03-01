79bf5058d6357d55b5e28bac5da90b63e077c0ab
Jin Hao Chong <chong900208@hotmail.com>
2012-03-01 18:47:18 -0500

    added timestamp and sort group list

--- Token.java
+++ Token.java
@@ -7,14 +7,16 @@ public class Token implements UserToken,java.io.Serializable{
 	 * 
 	 */
 	private static final long serialVersionUID = 1L;
-	String serverName, username;
+	String serverName, username, timestamp;
 	ArrayList<String> userGroups = new ArrayList<String>();
 	
-	public Token(String serverName, String username, ArrayList<String> userGroups){
+	public Token(String serverName, String username, ArrayList<String> userGroups, String timestamp){
 		// TODO complete here
 		this.serverName = serverName;
 		this.username = username;
 		this.userGroups = userGroups;
+		sort(this.userGroups);
+		this.timestamp = timestamp;
 	}
 
 	@Override
@@ -33,5 +35,35 @@ public class Token implements UserToken,java.io.Serializable{
 	public List<String> getGroups() {
 		// TODO Auto-generated method stub
 		return userGroups;
+	}
+	
+	public String getTimestamp(){
+		return timestamp;
+	}
+	
+	//get the combined string of all token data
+	public String getTokendata(){
+		return serverName + username + timestamp + userGroups.toString();
+	}
+	
+	//sort the arraylist
+	public void sort(ArrayList<String> list){
+		String small;
+		int least;
+		for(int i = 0; i < list.size() - 1; i++){
+			small = list.get(i);
+			least = i;
+			for(int j = i + 1; j < list.size(); j++){
+				if(list.get(j).compareTo(small) < 1){
+					small = list.get(j);
+					least = j;
+				}
+			}
+			if(least != i){
+				String temp = list.get(i);
+				list.set(i, small);
+				list.set(least, temp);
+			}
+		}
 	}
 }
